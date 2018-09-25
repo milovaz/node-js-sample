6a4cae074a6c1d4a68a9e39fe2c435cc93a7e178
Thiago Milo <milovaz@gmail.com>
2018-09-25 20:47:05 -0300

    changing service fabric artifacts folder

--- nodeSample/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sfctl application upload --path nodeSample --show-progress
-sfctl application provision --application-type-build-path nodeSample
-sfctl application create --app-name fabric:/nodeSample --app-type nodeSampleType --app-version 1.0.0
