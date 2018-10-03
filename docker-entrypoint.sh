369f55cdc6db841d9463e484454e632fcb24837c
Thiago Milo <milovaz@gmail.com>
2018-10-03 11:34:51 -0300

    including decrypt code

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 export PORT="${Fabric_Endpoint_nodeSampleEndpoint}"
-PARAPHRASEDEC=$(echo "${PARAPHRASE}" | openssl enc -d -base64 | openssl rsautl -decrypt -inkey "${Certificates_nodeSampleServicePkg_code_MyCert1_PEM}"))
+PARAPHRASEDEC=$(echo "${PARAPHRASE}" | openssl enc -d -base64 | openssl rsautl -decrypt -inkey "${Certificates_nodeSampleServicePkg_code_MyCert1_PEM}")
 export JENKINS_NAME_TESTE=$(echo "${JENKINS_NAME_TESTE}" | openssl enc -d -aes-256-cbc -a -k "${PARAPHRASEDEC}")
 npm start
